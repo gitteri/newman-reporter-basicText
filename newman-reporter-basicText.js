4c4deee50532f02fc05acffa00a612818cd18e97
Ilan Gitter <ilan.gitter@gmail.com>
2018-01-10 13:51:10 -0500

    Create newman-reporter-basicText.js

---
+++ newman-reporter-basicText.js
@@ -0,0 +1,43 @@
+module.exports = function(newman, repoterOptions) {
+    console.log('Running!');
+
+    let basicOutput = '';
+
+    newman.on('start', () => {
+        basicOutput += `Start collection run for collection "${newmanCollection.name}" (${newmanCollection.id})\n`;
+        this.count = 1;
+    });
+
+    newman.on('beforeItem', (err, o) => { });
+
+    newman.on('beforeRequest', (err, o) => { });
+
+    newman.on('request', (err, o) => { });
+
+    newman.on('script', (err, o) => { });
+
+    newman.on('assertion', (err, o) => {
+        if (err) {
+            basicOutput += `✗ Assertion failed! [${this.count} / ${o.item.name}]: "${o.assertion}"\n`;
+        } else {
+            basicOutput += ` ✔ Assertion passed! [${this.count} / ${o.item.name}]: "${o.assertion}"\n`;
+        }
+
+        this.count++;
+    });
+
+    newman.on('beforeDone', (err) => {
+        if (err) {
+            console.log('there was an error');
+            return;
+        }
+
+        basicOutput += `Collection run completed for collection "${newmanCollection.name}" (${newmanCollection.id}):  ${this.count} tests executed\n`;
+        newman.exports.push({
+            name: 'basic-reporter',
+            default: 'newman-run-report.txt',
+            path: reporterOptions.export,
+            content: basicOutput
+        });
+    });
+}
