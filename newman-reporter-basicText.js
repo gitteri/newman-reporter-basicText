57b2fae1ed02c5d732491dd0a8d45450da047f0c
Ilan Gitter <ilangitter@Paxos-Ilan-Gitter.local>
2018-01-12 10:58:30 -0500

    check for available vars

--- newman-reporter-basicText.js
+++ newman-reporter-basicText.js
@@ -23,12 +23,7 @@ module.exports = function(newman, reporterOptions) {
     newman.on('beforeRequest', (err, o) => { });
 
     newman.on('request', (err, o) => {
-        if (o.statusCode < 200 || o.statusCode > 300) {
-            log("✗ Request failed!\nResponse headers:\n" +
-                JSON.stringify(o.headers, undefined, 1) +
-                "\nResponse body:\n" + o.body + "\n"
-            );
-        }
+        log(JSON.stringify(o))
     });
 
     newman.on('script', (err, o) => { });
@@ -36,8 +31,7 @@ module.exports = function(newman, reporterOptions) {
     newman.on('assertion', (err, o) => {
         if (err) {
             log(`✗ Assertion failed! [${this.count} / ${o.item.name}] at ${new Date()}: "${o.assertion}"\n`);
-            log(`Message:\n${o.message}\n`);
-            log(`Stack:\n${o.stack}\n`);
+            log(`Message:\n${JSON.stringify(o)}\n`);
         } else {
             log(` ✔ Assertion passed! [${this.count} / ${o.item.name}]: "${o.assertion}"\n`);
         }
